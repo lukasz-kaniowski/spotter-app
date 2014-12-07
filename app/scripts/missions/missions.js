e36162fafeec029412d86d0a0ad0e905fc6a3db5
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2014-12-07 21:30:19 +0000

    activate modal

--- app/scripts/missions/missions.js
+++ app/scripts/missions/missions.js
@@ -22,5 +22,10 @@ angular
           }
         }
 
+      })
+
+      .state('missionActivate', {
+        url: '/activate',
+        templateUrl: 'scripts/missions/activate/activate.modal.html'
       });
   });
