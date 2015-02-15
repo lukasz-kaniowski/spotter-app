c0513d9b5e1f7bd6e43e70bfa26a625e67d65366
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2015-02-15 19:37:39 +0100

    fix retieving user to correct api endpoing

--- app/scripts/components/auth/user.service.js
+++ app/scripts/components/auth/user.service.js
@@ -1,22 +1,22 @@
 'use strict';
 
 angular.module('SpotterApp')
-  .factory('User', function ($resource) {
-    return $resource('/api/users/:id/:controller', {
-      id: '@_id'
-    },
-    {
-      changePassword: {
-        method: 'PUT',
-        params: {
-          controller:'password'
-        }
+  .factory('User', function ($resource, CONFIG) {
+    return $resource(CONFIG.apiEndpoint + '/api/users/:id/:controller', {
+        id: '@_id'
       },
-      get: {
-        method: 'GET',
-        params: {
-          id:'me'
+      {
+        changePassword: {
+          method: 'PUT',
+          params: {
+            controller: 'password'
+          }
+        },
+        get: {
+          method: 'GET',
+          params: {
+            id: 'me'
+          }
         }
-      }
-	  });
+      });
   });
