699deaebd88c75d6da252fd71ee9db3ff02355da
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2015-03-11 20:40:19 +0000

    fix due date display on mission list

--- app/scripts/missions/missions.decorator.js
+++ app/scripts/missions/missions.decorator.js
@@ -3,19 +3,14 @@
 angular.module('SpotterApp').factory('missionsDecorator', function() {
 	var decorateListMissions = function(missions) {
     missions.forEach(function (mission) {
-      var dueDate = new Date(missions.dueDate);
+      var dueDate = new Date(mission.dueDate);
       var curDate = new Date();
       mission.daysLeft = parseInt((dueDate-curDate)/(24*3600*1000));
 
     });
     return missions;
-		//for (var i in missions) {
-		//	var dueDate = new Date(missions[i].dueDate);
-		//	var curDate = new Date();
-		//	missions[i].daysLeft =
-		//}
-		//return missions;
 	};
+
 	return {
 		decorate : function(action, missions) {
       console.log('mission missions', missions);
