fcf41c0a2e499ce2b291a11d06f5930ced65f97c
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2015-05-03 12:14:48 +0100

    formatting

--- app/scripts/missions/tasks/answer/tasks-answer.ctrl.js
+++ app/scripts/missions/tasks/answer/tasks-answer.ctrl.js
@@ -1,44 +1,44 @@
 'use strict';
 
 angular.module('SpotterApp.missions')
-  .controller('TasksAnswerCtrl', function($scope, $ionicPlatform, $ionicActionSheet, imageService, $state, $ionicLoading, $ionicSlideBoxDelegate, $ionicModal, missionsService, mission, $stateParams, $localStorage){
+  .controller('TasksAnswerCtrl', function ($scope, $ionicPlatform, $ionicActionSheet, imageService, $state, $ionicLoading, $ionicSlideBoxDelegate, $ionicModal, missionsService, mission, $stateParams, $localStorage) {
 
     $scope.answers = {};
     $scope.mission = mission;
     $scope.startSlide = $stateParams.slide_num;
     var current_task_id, saved_answers = $localStorage[$stateParams.mission_id];
-    if(saved_answers){
+    if (saved_answers) {
       angular.copy(saved_answers, $scope.answers);
     }
-    var addImage = function(local_path) {
+    var addImage = function (local_path) {
       if (!local_path)
         return false;
       $scope.answers[current_task_id] = {url: local_path};
       $scope.$apply();
     };
-    $scope.saveData = function(){
-      if(!angular.equals($scope.answers, saved_answers)){
+    $scope.saveData = function () {
+      if (!angular.equals($scope.answers, saved_answers)) {
         $localStorage[$stateParams.mission_id] = $scope.answers;
         angular.copy($scope.answers, saved_answers);
       }
-      if($ionicSlideBoxDelegate.currentIndex() == ($ionicSlideBoxDelegate.slidesCount() - 1))
+      if ($ionicSlideBoxDelegate.currentIndex() == ($ionicSlideBoxDelegate.slidesCount() - 1))
         $state.go('app.tasks_list', {mission_id: $scope.mission._id});
       else
         $ionicSlideBoxDelegate.next();
-    }
-    $scope.showActionsheet = function(task_id) {
+    };
+    $scope.showActionsheet = function (task_id) {
       current_task_id = task_id;
       $ionicActionSheet.show({
         titleText: 'Select from',
         buttons: [
           {text: 'Camera'},
-          {text: 'Gallery'},
+          {text: 'Gallery'}
         ],
         cancelText: 'Cancel',
-        cancel: function() {
+        cancel: function () {
           return false;
         },
-        buttonClicked: function(index) {
+        buttonClicked: function (index) {
           if (index === 0) {
             imageService.camera(addImage);
             return true;
@@ -51,7 +51,7 @@ angular.module('SpotterApp.missions')
       });
     };
 
-    angular.element(document).ready(function(){
+    angular.element(document).ready(function () {
       $ionicSlideBoxDelegate.$getByHandle('tasks').enableSlide(false);
     });
 
